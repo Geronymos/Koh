caf2064194632eee5f97ea00ae76745d340a1421
Geronymos <spielerbereich@gmail.com>
2021-07-15 23:51:25 +0200

    match image frm data set to camera

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 import cv2
 import mediapipe as mp
+import json
+import math
+
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
-# For static images:
-IMAGE_FILES = []
-drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
-with mp_face_mesh.FaceMesh(
-    static_image_mode=True,
-    max_num_faces=1,
-    min_detection_confidence=0.5) as face_mesh:
-  for idx, file in enumerate(IMAGE_FILES):
-    image = cv2.imread(file)
-    # Convert the BGR image to RGB before processing.
-    results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    # Print and draw face mesh landmarks on the image.
-    if not results.multi_face_landmarks:
-      continue
-    annotated_image = image.copy()
-    for face_landmarks in results.multi_face_landmarks:
-      print('face_landmarks:', face_landmarks)
-      mp_drawing.draw_landmarks(
-          image=annotated_image,
-          landmark_list=face_landmarks,
-          connections=mp_face_mesh.FACE_CONNECTIONS,
-          landmark_drawing_spec=drawing_spec,
-          connection_drawing_spec=drawing_spec)
-    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
+with open("faces.json", "r") as file:
+  images_faces = json.load(file)
+
+def match_faces(face1, face2):
+  # dist1 = [[landmark["x"], landmark["y"], landmark["z"]] for landmark in face1]
+  dist1 = [[landmark["x"], landmark["y"]] for landmark in face1]
+  dist1 = [item for sublist in dist1 for item in sublist]
+  # print(len(dist1))
+
+  # dist2 = [[landmark["x"], landmark["y"], landmark["z"]] for landmark in face2]
+  dist2 = [[landmark["x"], landmark["y"]] for landmark in face2]
+  dist2 = [item for sublist in dist2 for item in sublist]
+  # print(len(dist2))
+
+  return math.dist(dist1, dist2)
+
+# For static images:
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 # For webcam input:
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 cap = cv2.VideoCapture(0)
 with mp_face_mesh.FaceMesh(
+    max_num_faces=1,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as face_mesh:
+    # min_tracking_confidence=0.5
+    ) as face_mesh:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
@@ -55,6 +54,18 @@ with mp_face_mesh.FaceMesh(
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_face_landmarks:
       for face_landmarks in results.multi_face_landmarks:
+
+        face_dict = [{"x": landmark.x, "y": landmark.y, "z": landmark.z} for landmark in face_landmarks.landmark]
+
+        # print(face_dict)
+
+        sorted_images = sorted(images_faces, key=lambda elem: match_faces(elem["faces"][0], face_dict))
+        top_image = sorted_images[0]["file"]
+        print(top_image)
+
+        image_match = cv2.imread(top_image)
+        cv2.imshow('Selected image', image_match)
+
         mp_drawing.draw_landmarks(
             image=image,
             landmark_list=face_landmarks,
